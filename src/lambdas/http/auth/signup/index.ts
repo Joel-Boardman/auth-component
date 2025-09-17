0a44f5262976ad2d3ad5a87d4b2b549c7c25d510
Joel-Boardman <joel.gilmore97@gmail.com>
2025-09-17 20:22:27 +0100

    base set up complete. Most likely will change in future with further changes

---
+++ src/lambdas/http/auth/signup/index.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ hello: "world" }),
+  };
+};

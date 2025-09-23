6145961ec93200c381cfb6994576a880fe0b8263
Joel-Boardman <joel.gilmore97@gmail.com>
2025-09-23 17:00:16 +0100

    test: handle check verification

--- src/lambdas/http/auth/postConfirmation/index.test.ts
+++ src/lambdas/http/auth/postConfirmation/index.test.ts
@@ -1,5 +1,11 @@
 import generateApiGatewayEvent from "../../../../../testing-tools/generateApiGatewayEvent";
 import postConfirmation from "./";
+import { cognitoAdminGetUser } from "../../../../services/cognito";
+import { InternalServerError } from "../../../../utils/errors";
+
+jest.mock("../../../../services/cognito");
+
+const mockCognitoAdminGetUser = cognitoAdminGetUser as jest.Mock;
 
 describe("PostConfirmation", () => {
   describe("WHEN the Request payload is invalid", () => {
@@ -16,6 +22,28 @@ describe("PostConfirmation", () => {
       expect(res.body).toBe(
         JSON.stringify({ message: "Invalid request body" })
       );
+    });
+  });
+
+  describe("WHEN Cognito is called to check for verification", () => {
+    describe("AND the SDK call fails", () => {
+      it("SHOULD return a 500 Internal Server Error", async () => {
+        mockCognitoAdminGetUser.mockRejectedValue(
+          new InternalServerError("Internal server error")
+        );
+
+        const event = generateApiGatewayEvent("POST", "/post-confirmation", {
+          body: {
+            userId: "aaaaa-bbbbb-ccccc-ddddd-eeeee-ffffff",
+          },
+        });
+        const res = await postConfirmation(event);
+
+        expect(res.statusCode).toBe(500);
+        expect(res.body).toBe(
+          JSON.stringify({ message: "Internal server error" })
+        );
+      });
     });
   });
 });
--- src/services/cognito/index.ts
+++ src/services/cognito/index.ts
@@ -1,8 +1,9 @@
 import { CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-provider";
 import { cognitoCreateUser } from "./createUser";
+import { cognitoAdminGetUser } from "./adminGetUser";
 
 const config = {};
 
 const cognitoIdentityClient = new CognitoIdentityProviderClient(config);
 
-export { cognitoIdentityClient, cognitoCreateUser };
+export { cognitoIdentityClient, cognitoCreateUser, cognitoAdminGetUser };
